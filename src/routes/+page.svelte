7a5bb4972269a9ef1c373f5da4cf7a1a6ff29f47
Sam Zanca <sam@zanca.dev>
2023-02-19 12:57:57 -0500

    draft

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import rss from './api/rss.json'
+</script>
+
+<div class="container">
+  <main>
+    <h1>Tech Influencer Aggregator</h1>
+    
+    <p>
+      This app is in idea phase.
+    </p>
+    
+    <p>
+      Going to make aggregate all the tech channels I enjoy and wrap them up in rss groups that can be subscribed to. Either using tags are individually.
+    </p>
+    
+    <p>Heres the dataset draft</p>
+    
+  </main>
+</div>
+
+<pre>{JSON.stringify(rss, null, 2)}</pre>
+
+<style>
+  * {
+    padding: 0;
+    /* margin: 0; */
+    box-sizing: border-box;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+  }
+
+  main {
+    max-width: 600px;
+  }
+</style>
